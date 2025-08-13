c21c865a37e764838d9ee204e8489b726134eac0
michelmoura1989 <124470627+michelmoura1989@users.noreply.github.com>
2025-08-12 23:51:10 -0300

    Adicionando referencias

--- app/src/main/java/com/example/calculadoimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoimc/MainActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val edtPeso = findViewById<EditText>(R.id.edtPeso)
+        val edtAltura = findViewById<EditText>(R.id.edtAltura)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupSexo)
     }
 }
