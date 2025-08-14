193f7fd5c0168f39b3fba256803adae558210e22
michelmoura1989 <124470627+michelmoura1989@users.noreply.github.com>
2025-08-14 00:19:22 -0300

    adicionando botoes ao codigo

--- app/src/main/java/com/example/calculadoimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoimc/MainActivity.kt
@@ -7,13 +7,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         val edtPeso = findViewById<EditText>(R.id.edtPeso)
         val edtAltura = findViewById<EditText>(R.id.edtAltura)
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroupSexo)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val btnLimpar = findViewById<Button>(R.id.btnLimpar)
+        val txtResultado = findViewById<TextView>(R.id.textResultado)
     }
 }
