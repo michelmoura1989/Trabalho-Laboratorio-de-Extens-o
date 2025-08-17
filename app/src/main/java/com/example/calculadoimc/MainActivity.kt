fa669e367d28dc02c9e01d1452479474d9dc1afa
michelmoura1989 <124470627+michelmoura1989@users.noreply.github.com>
2025-08-17 00:02:28 -0300

    adicionando a lógica de cálculo

--- app/src/main/java/com/example/calculadoimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoimc/MainActivity.kt
@@ -21,5 +21,25 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btnCalcular)
         val btnLimpar = findViewById<Button>(R.id.btnLimpar)
         val txtResultado = findViewById<TextView>(R.id.textResultado)
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text.toString().toFloatOrNull()
+            val altura = edtAltura.text.toString().toFloatOrNull()
+
+            if (peso != null && altura != null) {
+                val imc = peso / (altura * altura)
+                val classificacao = when {
+                    imc < 18.5 -> "Abaixo do peso"
+                    imc < 24.9 -> "Peso normal"
+                    imc < 29.9 -> "Sobrepeso"
+                    imc < 34.9 -> "Obesidade grau I"
+                    imc < 39.9 -> "Obesidade grau II"
+                    else -> "Obesidade grau III"
+                }
+                txtResultado.text = "IMC: %.2f\n$classificacao".format(imc)
+            } else {
+                txtResultado.text = "Preencha os campos corretamente"
+            }
+        }
     }
+
 }
